1c82912ffbc03cb5ec17328e10560fe643841b5b
Ming Li <headhuntar@gmail.com>
2018-07-30 18:58:00 -0700

    First commit: Project Learning swift

--- LearningSwift/ViewController.swift
+++ LearningSwift/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showMessage(send: UIButton) {
+        let alertController = UIAlertController(title: "First swift project example", message: "Button Click", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
