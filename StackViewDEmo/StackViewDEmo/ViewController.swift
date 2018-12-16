71d48a3b630fe4433b89ee9fdd8652b4386d434a
Allen Li <allenlidev@gmail.com>
2018-12-15 17:57:36 -0800

    working with stack views and landscape constraints and adaptive layout

---
+++ StackViewDEmo/StackViewDEmo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDEmo
+//
+//  Created by Ming Li on 2018-12-15.
+//  Copyright © 2018 Allenlidev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
