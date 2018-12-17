04ebf9717a1a458717c0383168e96ed4403c6688
Allen Li <allenlidev@gmail.com>
2018-12-16 19:18:48 -0800

    project to learn table view

---
+++ TableView/TableView/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Ming Li on 2018-12-16.
+//  Copyright © 2018 Allenlidev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var restaurantNames = ["Tokyo Joes", "Minami", "Sushi California", "LukLak", "PokPok"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "datacell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
