07f2e5d47427025aaa457d5cf82d5b3684f72116
Allen Li <allenlidev@gmail.com>
2018-12-24 17:41:00 -0800

    created custom view for cell

---
+++ FoodPin/FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Ming Li on 2018-12-24.
+//  Copyright © 2018 Allenlidev. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
